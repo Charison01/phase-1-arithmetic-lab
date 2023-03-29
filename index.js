c3e4fc2fd69325206c584b52776befd970e5db4d
Kelvin <kelvinkimaru@gmail.com>
2023-03-29 15:48:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1*num2;
+
+function randomInteger() {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+}
+const random = randomInteger();
+
+let num3 = 30;
+let num4 = 13;
+const mod = num3%num4;
+
+const numbers = [10,16,9,20];
+const max = Math.max(...numbers);
+/*
+function random() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+*/
+/*
+function multiply(num1,num2){
+    return num1*num2;
+}
+let result = multiply(num1,num2);
+*/
